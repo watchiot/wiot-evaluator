597909b9e66316290671e1d6455d2d6d50a6b4f6
gorums <acksecurity@hotmail.com>
2016-04-10 02:26:14 -0500

    add dependency

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in wiot-parser.gemspec
 gemspec
+
+gem 'antlr3', '~> 1.10'
--- spec/wiot/parser_spec.rb
+++ spec/wiot/parser_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'wiot/parser/version'
 
 describe Wiot::Parser do
   it 'has a version number' do
@@ -8,13 +9,13 @@ describe Wiot::Parser do
   it 'valid yaml with nil constrainers' do
     errors = Wiot::Parser.parse('yaml', nil)
     expect(errors).not_to be_nil
-    expect(errors.length).to eq(2)
+    expect(errors.length).to eq(1)
   end
 
   it 'valid yaml with nil constrainers' do
     tokens = Wiot::Parser.token
     expect(tokens).not_to be_nil
 
-    expect(tokens.length).to eq(3)
+    expect(tokens.length).to eq(11)
   end
 end
